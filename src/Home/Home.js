b898e364aa2dcf1804b46c3687854775eca5df41
unknown <aneesa.farook.sait@gmail.com>
2020-09-30 18:01:14 +0530

    :sparkles: Banner component with search date feature added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,7 +5,6 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home Component</h1>
       <Banner />
     </div>
   );
---
+++ src/Search/Search.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { DateRangePicker } from "react-date-range";
+import PeopleIcon from "@material-ui/icons/People";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import "./Search.css";
+import { Button } from "@material-ui/core";
+
+const Search = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  };
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+  return (
+    <div className="search">
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+      <h2>
+        Number of guests <PeopleIcon />
+      </h2>
+      <input min={0} defaultValue={2} />
+      <Button>Search Airbnb</Button>
+    </div>
+  );
+};
+
+export default Search;
